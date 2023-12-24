5534e46cd9a96620efc29996e0a260959b32b681
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-12-24 15:59:01 +0900

    Design: 비로그인 시 메인 화면 구현

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Container, Image } from "react-bootstrap";
 
 const StartPage = () => {
-  return <div>StartPage</div>;
+  return (
+    <Container className="text-center">
+      <Image
+        src={process.env.PUBLIC_URL + "/image/Search.gif"}
+        fluid
+        width={500}
+      />
+    </Container>
+  );
 };
 
 export default StartPage;
